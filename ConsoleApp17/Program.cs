7280b0d75b0dabb8e5a41541d8476a9a89908e98
Mahammad2255 <[mahammadei@code.edu.az]>
2021-12-11 05:48:46 -0800

    Add project files.

---
+++ ConsoleApp17/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsoleApp17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 4, 8, 2, 5, 6, 48, 51,56,90 };
+            for (int i = arr.Length - 1; i>=0 ; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
